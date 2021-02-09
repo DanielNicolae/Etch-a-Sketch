952004abb3cdf97e1a5591b720697f55e4f52804
Daniel Nicolae <DeadlyPoet@yandex.com>
2021-02-09 11:31:31 +0200

    I solved the bug related to the reset button

--- index.js
+++ index.js
@@ -2,26 +2,34 @@
 let grid = document.getElementById("grid");
 let rgba = "";
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        if ((i + j) % 2 === 0) {
-            let cell = document.createElement("div");
-            cell.classList.add("even");
-            cell.addEventListener("mouseover", () => {
-                cell.setAttribute("style", `background: rgba(${rgba[0]},${rgba[1]},${rgba[2]},${rgba[3]}) !important;`);
-            });
-            grid.appendChild(cell);
-        }
-        else {
-            let cell = document.createElement("div");
-            cell.classList.add("odd");
-            cell.addEventListener("mouseover", () => {
-                cell.setAttribute("style", `background: rgba(${rgba[0]},${rgba[1]},${rgba[2]},${rgba[3]}) !important;`);
-            });
-            grid.appendChild(cell);
+function evenOddCells() {
+    let rows = getComputedStyle(document.body).getPropertyValue("--rows");
+    let columns = getComputedStyle(document.body).getPropertyValue("--columns");
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            if ((i + j) % 2 === 0) {
+                let cell = document.createElement("div");
+                cell.classList.add("even");
+                cell.addEventListener("mouseover", () => {
+                    cell.setAttribute("style", `background: rgba(${rgba[0]},${rgba[1]},${rgba[2]},${rgba[3]}) !important;`);
+                });
+                grid.appendChild(cell);
+            }
+            else {
+                let cell = document.createElement("div");
+                cell.classList.add("odd");
+                cell.addEventListener("mouseover", () => {
+                    cell.setAttribute("style", `background: rgba(${rgba[0]},${rgba[1]},${rgba[2]},${rgba[3]}) !important;`);
+                });
+                grid.appendChild(cell);
+            }
         }
     }
+
 }
+
+evenOddCells();
+
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
@@ -33,23 +41,17 @@ let clicked = false;
 let button = document.getElementById("reset");
 button.addEventListener("click", () => {
     removeAllChildNodes(grid);
-    let rows = prompt("Choose the number of rows:");
-    let columns = prompt("Choose the number of columns:");
-    document.documentElement.style.setProperty("--rows", rows);
-    document.documentElement.style.setProperty("--columns", columns);
-    if (rows <= 100 && columns <= 100) {
-        for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < columns; j++) {
-                let cell = document.createElement("div");
-                cell.addEventListener("mouseover", () => {
-                    cell.classList.add("cellHover");
-                });
-                grid.appendChild(cell);
-            }
-        }
+    let newRows = prompt("Choose the number of rows:");
+    let newColumns = prompt("Choose the number of columns:");
+    document.documentElement.style.setProperty("--rows", newRows);
+    document.documentElement.style.setProperty("--columns", newColumns);
+    console.log(newRows);
+    console.log(newColumns)
+    if (newRows <= 100 && newColumns <= 100) {
+        evenOddCells();
     }
     else {
-        alert("The maximum number of rows or columns is 100.")
+        alert("The maximum number of rows or columns is 100.");
     }
     clicked = !clicked;
     if (clicked) {
